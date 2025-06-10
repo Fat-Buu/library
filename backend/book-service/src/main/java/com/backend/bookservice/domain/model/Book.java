72b1044071f1e5844b71383cc6b9f7908b6186d3
Apisit <amornchanchaigul@gmaul.com>
2025-06-10 23:07:22 +0700

    update book-service

--- backend/book-service/src/main/java/com/backend/bookservice/domain/model/Book.java
+++ backend/book-service/src/main/java/com/backend/bookservice/domain/model/Book.java
@@ -1,10 +1,13 @@
 package com.backend.bookservice.domain.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -15,5 +18,14 @@ public class Book {
     @Id
     private String id;
     private String name;
+    private String author;
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+    @Column(name = "create_by")
+    private String createBy;
+    @Column(name = "update_date")
+    private LocalDateTime updateDate;
+    @Column(name = "update_by")
+    private String updateBy;
 
 }
--- backend/book-service/src/main/java/com/backend/bookservice/web/dto/BookDto.java
+++ backend/book-service/src/main/java/com/backend/bookservice/web/dto/BookDto.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,4 +13,8 @@ public class BookDto {
     private String id;
     private String name;
     private String author;
+    private LocalDateTime createDate;
+    private String createBy;
+    private LocalDateTime updateDate;
+    private String updateBy;
 }
