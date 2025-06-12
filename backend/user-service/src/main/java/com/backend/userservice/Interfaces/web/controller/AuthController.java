b87443d20bc93b0e61a813e67bc459ba855341fe
Apisit <amornchanchaigul@gmaul.com>
2025-06-12 21:59:14 +0700

    implement Docker

--- backend/user-service/src/main/java/com/backend/userservice/Interfaces/web/controller/AuthController.java
+++ backend/user-service/src/main/java/com/backend/userservice/Interfaces/web/controller/AuthController.java
@@ -1,14 +1,13 @@
 package com.backend.userservice.interfaces.web.controller;
 
 import com.backend.userservice.infrastructure.security.JwtUtil;
+import com.backend.userservice.interfaces.web.dto.request.UserLoginRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/v1/auth")
@@ -19,8 +18,8 @@ public class AuthController {
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody Map<String, String> body) {
-        String token = jwtUtil.generateToken(body.get("username"));
+    public ResponseEntity<String> login(@RequestBody UserLoginRequest userLogin) {
+        String token = jwtUtil.generateToken(userLogin.getUserName());
         return ResponseEntity.ok(token);
     }
 
